893e01ffe01aedddfec5a3fe3f611f26e619bdd9
teresarico18 <teresa.rico@iest.edu.mx>
2022-05-12 15:30:50 -0500

    Add register method on eventCard

--- frontend/src/components/EventCard/index.js
+++ frontend/src/components/EventCard/index.js
@@ -1,8 +1,10 @@
-import React from "react";
-import "./EventCard.scss";
+import React from 'react';
+import './EventCard.scss';
+import api from '../../services/api';
 
 const EventCard = ({
   data: {
+    _id,
     title,
     description,
     date,
@@ -15,38 +17,50 @@ const EventCard = ({
     eventType,
   },
 }) => {
-
   const dateOptions = {
-    year: "numeric",
-    month: "long",
-    day: "numeric",
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
   };
-  
-  const handleClick = () => {
-    // do thing on backend heheheh
-    console.log("HEREEEE");
-  }
+
+  const register = async () => {
+    const confirmation = window.confirm(
+      '¿Estás seguro que deseas registrarte a este evento?'
+    );
+    if (confirmation == true) {
+      const response = await api.put(`/events/${_id}/attendees/`, {
+        id: localStorage.getItem('id'),
+        idIest: localStorage.getItem('idIest'),
+        name: localStorage.getItem('name'),
+        lastNames: localStorage.getItem('lastNames'),
+        attended: false,
+        points: 0,
+      });
+      console.log(response.data.message);
+      alert(response.data.message);
+    }
+  };
 
   return (
-    <div className="event-container" onClick={handleClick}>
-      <div className="event-container--columns">
-        <p className="event-container__type">{eventType}</p>
-        <p>Fecha: {new Date(date).toLocaleDateString("es-MX", dateOptions)}</p>
+    <div className='event-container' onClick={register}>
+      <div className='event-container--columns'>
+        <p className='event-container__type'>{eventType}</p>
+        <p>Fecha: {new Date(date).toLocaleDateString('es-MX', dateOptions)}</p>
       </div>
       <img src={img} />
-      <p className="event-container__title">{title}</p>
-      <div className="event-container--columns">
+      <p className='event-container__title'>{title}</p>
+      <div className='event-container--columns'>
         <p>Cupo: {availability}</p>
         <p>Generación: {generation}</p>
       </div>
-      <div className="event-container--columns">
+      <div className='event-container--columns'>
         <p>Comité: {committee}</p>
         <p>Lugar: {place}</p>
       </div>
-      <div className="event-container--columns">
+      <div className='event-container--columns'>
         <p>Modalidad: {modality}</p>
       </div>
-      <p className="event-container__description">{description}</p>
+      <p className='event-container__description'>{description}</p>
     </div>
   );
 };
