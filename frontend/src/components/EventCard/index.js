c6f2a6abe3c67489d1c21ac802c976113efb0e40
andreassalazar <andrea.sosa01@iest.edu.mx>
2022-10-01 17:23:34 -0500

    Add style to register and cancel buttons with new click

--- frontend/src/components/EventCard/index.js
+++ frontend/src/components/EventCard/index.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
-import './EventCard.scss';
-import api from '../../services/api';
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import "./EventCard.scss";
+import api from "../../services/api";
 
 const EventCard = ({
   eventId,
@@ -24,43 +24,44 @@ const EventCard = ({
   const navigate = useNavigate();
 
   const dateOptions = {
-    year: 'numeric',
-    month: 'long',
-    day: 'numeric',
+    year: "numeric",
+    month: "long",
+    day: "numeric",
   };
 
   const handleClick = async () => {
     // si esta en el modo admin entonces lo redigira a la asistencia
-    if (currentLocation === '/dashboard/eventos') {
+    if (currentLocation === "/dashboard/eventos") {
       navigate(`/dashboard/eventos/${eventId}/asistencias`);
       return;
     }
     const confirmation = window.confirm(
-      '¿Estás seguro que deseas registrarte a este evento?'
+      "¿Estás seguro que deseas registrarte a este evento?"
     );
     if (confirmation == true) {
       const response = await api.put(`/events/${_id}/attendees/`, {
-        id: localStorage.getItem('id'),
-        idIest: localStorage.getItem('idIest'),
-        gen: localStorage.getItem('gen'),
-        name: localStorage.getItem('name'),
-        lastNames: localStorage.getItem('lastNames'),
+        id: localStorage.getItem("id"),
+        idIest: localStorage.getItem("idIest"),
+        gen: localStorage.getItem("gen"),
+        name: localStorage.getItem("name"),
+        lastNames: localStorage.getItem("lastNames"),
         attended: false,
         points: 0,
       });
       console.log(response.data.message);
       alert(response.data.message);
+      window.location.reload();
     }
   };
 
   const isMemberRegistered = () => {
     console.log(attendees);
-    var member = attendees.find(
-      (member) => member.id === localStorage.getItem('id')
+    let member = attendees.find(
+      (member) => member.id === localStorage.getItem("id")
     );
     //cuando esta registrado es true
     if (member !== undefined) {
-      console.log('esta registrado');
+      console.log("esta registrado");
       console.log(member !== undefined);
       return member !== undefined;
     }
@@ -73,7 +74,7 @@ const EventCard = ({
     );
     if (confirmation == true) {
       const response = await api.put(
-        `/events/${_id}/attendees/remove/${localStorage.getItem('id')}`
+        `/events/${_id}/attendees/remove/${localStorage.getItem("id")}`
       );
       console.log(response.data.message);
       alert(response.data.message);
@@ -82,36 +83,39 @@ const EventCard = ({
   };
 
   return (
-    <div className='event-container' onClick={handleClick}>
-      <div className='event-container--columns'>
-        <p className='event-container__type'>{eventType}</p>
-        <p>Fecha: {new Date(date).toLocaleDateString('es-MX', dateOptions)}</p>
+    <div className="event-container">
+      <div className="event-container--columns">
+        <p className="event-container__type">{eventType}</p>
+        <p>Fecha: {new Date(date).toLocaleDateString("es-MX", dateOptions)}</p>
       </div>
       <img src={img} />
-      <p className='event-container__title'>{title}</p>
-      <div className='event-container--columns'>
+      <p className="event-container__title">{title}</p>
+      <div className="event-container--columns">
         <p>Cupo: {availability}</p>
         <p>Modalidad: {modality}</p>
-
-        {/* <p>Generación: {generation}</p> */}
       </div>
-      <div className='event-container--columns'>
-        <p id='event-container__committee'>
-          Comité:{' '}
+      <div className="event-container--columns">
+        <p id="event-container__committee">
+          Comité:{" "}
           {committee.map((c) => (
             <p>{c}</p>
           ))}
         </p>
         <p>Lugar: {place}</p>
       </div>
-      <p className='event-container__description'>{description}</p>
-      {isMemberRegistered() && (
+      <p className="event-container__description">{description}</p>
+      {isMemberRegistered() ? (
         <button
+          className="event-container__btn"
           onClick={() => {
             cancelAttendance();
           }}
         >
           Cancelar registro
+        </button>
+      ) : (
+        <button className="event-container__btn" onClick={handleClick}>
+          Registrarse
         </button>
       )}
     </div>
